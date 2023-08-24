b12207dc226d7f11e05ef876413bb5ff7a3d2e6b
bhatiadheeraj <dheerajbhatia2010@gmail.com>
2023-08-24 22:16:29 +0000

    added changes to the guard

---
+++ src/auth/guards/oauth.guards.ts
@@ -0,0 +1,27 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Response } from 'express';
+
+@Injectable()
+export class GoogleOauthGuard extends AuthGuard('google') {
+    handleRequest<TUser = any>(err: any, user: any, info: any, context: ExecutionContext, status?: any) {
+        if (err || !user) {
+            const res: Response = context.switchToHttp().getResponse();
+            res.redirect('/auth/#!/signin?msg='+"Failed to authenticate");
+            return;
+        }
+        return user;
+    }
+}
+
+@Injectable()
+export class GithubOauthGuard extends AuthGuard('github') {
+    handleRequest<TUser = any>(err: any, user: any, info: any, context: ExecutionContext, status?: any) {
+        if (err || !user) {
+            const res: Response = context.switchToHttp().getResponse();
+            res.redirect('/auth/#!/signin?msg='+"Failed to authenticate");
+            return;
+        }
+        return user;
+    }
+}
