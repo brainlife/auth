3e18eb08f1df47049b703e9b19271811e543eae3
bhatiadheeraj <dheerajbhatia2010@gmail.com>
2023-08-31 22:24:17 +0000

    current WIP-> uses oauth directly for orcid

--- src/auth/strategy/orcid.strategy.ts
+++ src/auth/strategy/orcid.strategy.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-orcid";
+import { Strategy } from "passport-oauth2";
 import { AuthService } from "../auth.service";
 import { Profile } from "passport";
 
@@ -8,11 +8,13 @@ import { Profile } from "passport";
 export class OrcidStrategy extends PassportStrategy(Strategy, 'orcid') {
     constructor(private authService:AuthService) {
         super({
-            sandbox: process.env.NODE_ENV !== 'production', // use the sandbox for non-production environments
+            // sandbox: process.env.NODE_ENV !== 'production', // use the sandbox for non-production environments
             clientID: process.env.ORCID_CLIENT_ID,
             clientSecret: process.env.ORCID_CLIENT_SECRET,
             callbackURL: process.env.ORCID_CALLBACK_URL,
-            // scope: '/authenticate'
+            authorizationURL: process.env.ORCID_AUTHORIZATION_URL,
+            tokenURL: process.env.ORCID_TOKEN_URL,
+            scope: '/authenticate'
             // scope: ['given_names', 'family_names','email', 'orcid'],
             //https://info.orcid.org/documentation/integration-guide/customizing-the-sign-in-register-screen/#Pre-fill_the_registrationSign-in_form0
         });
