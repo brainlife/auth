4dd52d69a71fb3c27d2efe051938711081bfed52
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2023-10-18 17:49:42 -0500

    current State: able to add jwt to cookie

--- src/controller/github.controller.ts
+++ src/controller/github.controller.ts
@@ -39,89 +39,19 @@ export class GithubController {
   async callback(@Req() req: Request, @Res() res: Response) {
     // This route is protected by GitHub authentication
     // NestJS will automatically redirect the user to GitHub for authentication
-
     //TODO NEEDS DISCUSSION
     //req.user is usually parsed by jwt guard
     //but in this case, we are using github oauth guard which doesn't parse jwt but wraps
     //user object inside req.user.user
-
-    // but we see that the original api uses cookie and uses it to parse jwt
-    console.log('callback github');
-    const {
-      user: { user: userParsedfromGithub, profile },
-    } = req.user as any;
-
-    let userParsedfromCookie = null;
-    if (req.cookies['associate_jwt'])
-      userParsedfromCookie = decodeJWT(req.cookies['associate_jwt']);
-    console.log('github callback', userParsedfromGithub);
-
-    if (userParsedfromCookie) {
-      res.clearCookie('associate_jwt');
-      if (userParsedfromGithub) {
-        const messages = [
-          {
-            type: 'error',
-            message:
-              'Your github account is already associated to another account. Please signoff / login with your github account.',
-          },
-        ];
-        res.cookie('messages', JSON.stringify(messages), { path: '/' });
-        return res.redirect(settingsCallback);
-      }
-      const userRecord = await this.userService.findOne({
-        sub: userParsedfromCookie.sub,
-      });
-      if (!userRecord)
-        throw new Error(
-          "couldn't find user record with sub:" + userParsedfromCookie.sub,
-        );
-      userRecord.ext.github = profile.id;
-      await this.userService.updatebySub(userRecord.sub, userRecord);
-      // should i implement some method - DRY !! ??
-      const messages = [
-        {
-          type: 'success',
-          message: 'Successfully associated your github account',
-        },
-      ];
-      res.cookie('messages', JSON.stringify(messages), { path: '/' });
-      return res.redirect(settingsCallback);
-    } else {
-      if (!userParsedfromGithub) {
-        if (github.autoRegister) this.registerNewUser(profile, res);
-        else {
-          res.redirect(
-            '/auth/#!/signin?msg=' +
-              'Your github account is not yet registered. Please login using your username/password first, then associate your github account inside account settings.',
-          );
-        }
-        return;
-      }
-      if (checkUser(userParsedfromGithub, req)?.message)
-        return new Error(checkUser(userParsedfromGithub, req).message);
-      const claim = await createClaim(
-        userParsedfromGithub,
-        this.userService,
-        this.groupService,
-      );
-      userParsedfromGithub.times.github_login = new Date();
-      userParsedfromGithub.reqHeaders = req.headers;
-      await this.userService.updatebySub(
-        userParsedfromGithub.sub,
-        userParsedfromGithub,
-      );
-      this.publishToQueue.publishToQueue(
-        'user.login.' + userParsedfromGithub.sub,
-        JSON.stringify({
-          sub: userParsedfromGithub.sub,
-          source: 'github',
-          reqHeaders: req.headers,
-        }),
-      );
-      const jwt = signJWT(claim);
-      res.redirect('/auth/#!/success/' + jwt);
+    // Case 1 : user is already logged in (with jwt) and trying to associate github account with brainlife account (associate) from the account page -> associate it and point to account page
+    console.log("JWT", req.cookies.associate_jwt);
+    console.log("------------------------------------------------");
+    if (req.user) {
+      console.log('logged in user', req.user);
     }
+    // Case 2 : user is not logged in and trying to login with github account and has no account in brainlife (register) will point to register page
+    // Case 3 : if user has already an account linked in brainlife, it will just login and point to home page
+    // Case 4 : user A is already logged in (with local) and trying to associate github, but the github account it already associated to a brainlife user B it should send an error
   }
 
   registerNewUser(profile: any, res: Response) {
@@ -141,23 +71,38 @@ export class GithubController {
   }
 
   @Get('associate/:jwt')
-  @UseGuards(AuthGuard('github'))
-  async associateWithGithub(@Param('jwt') jwt: string, @Res() res: Response) {
+  // @UseGuards(AuthGuard('github'))
+  async associateWithGithub(
+    @Param('jwt') jwt: string,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    // This route is protected by GitHub authentication
+    // NestJS will automatically redirect the user to GitHub for authentication
+    console.log('associateWithGithub', jwt);
     try {
       const decodedToken = decodeJWT(jwt);
+      console.log('decodedToken', decodedToken);
       // Do any further checks if necessary, for example, you might check if a user exists in your system
       const user = await this.userService.findOne({ sub: decodedToken.sub });
       if (!user) throw new Error('User not found');
 
+      console.log('setting Cookie');
       res.cookie('associate_jwt', jwt, {
-        httpOnly: true,
-        secure: true,
+        httpOnly: false,
+        // secure: true,
         maxAge: 1000 * 60 * 5, //5 minutes
       });
+
+      // confirm the cookie is set
+      console.log('cookie SET', res.cookie);
       // This is where you'd typically continue with the GitHub association process...
-      this.signIn();
-    } catch (error) {
+      // After setting cookies, manually redirect to GitHub for authentication
       // Handle errors from the JWT decoding process, for example:
+
+      // redirect to github for authentication where it goes to callback
+
+      res.redirect('/api/auth/github/signin');
+    } catch (err) {
       res.status(401).send({ error: 'Invalid or expired token' });
     }
   }
